4da645296becf1f2593a091582ee96c976e48f1d
smnbbrv <smnbbrv@users.noreply.github.com>
2019-10-16 15:52:21 +0200

    fix: fix dry mode catching wrong line

--- release.sh
+++ release.sh
@@ -15,7 +15,7 @@ export GIT_CREDENTIALS=$(node /semantic-release/create-credentials.js)
 cp /semantic-release/.releaserc.json .releaserc
 
 if [ "$MODE" = "predict" ]; then
-  (semantic-release -d || exit 1) | sed -E 's/.*([0-9]+.[0-9]+.[0-9]+)/\1/' > .release-version
+  (semantic-release -d || exit 1) | grep 'Published release' | sed -E 's/.*([0-9]+.[0-9]+.[0-9]+)/\1/' > .release-version
 else
   semantic-release  || exit 1
 fi
